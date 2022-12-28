1b2858cb4e41c0ced3cd207b3ae6a16b69f799f6
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2022-12-28 13:24:23 +0300

    Add matrix polynomial

---
+++ MatrixPolynomial.cs
@@ -0,0 +1,33 @@
+class MatrixPolynomial
+{
+    /*
+    Класс Матричный полином
+    */
+
+    private double[] _data = new double[0];
+
+    // 1. Конструктор полиномов
+    public MatrixPolynomial(double[] coefficient)
+    {
+        this._data = coefficient;
+    }
+
+
+    public Matrix Calculate(Matrix matrix)
+    {
+        if (matrix.GetDimension()[0] != matrix.GetDimension()[1])
+        {
+            throw new Exception("Матричный полном должен состоять из квадратных матриц!");
+        }
+
+
+        Matrix res = new Matrix(matrix.GetDimension()[0], matrix.GetDimension()[1]);
+        InstanceThreadPool thread_pool = new InstanceThreadPool(4, Name: "Обработчик матриц");
+        for (int i = 0; i < this._data.Length; i++)
+        {
+            res = res.ParallelSumThreadPool(res.ParallelPowBinThreadPool((uint)i, thread_pool).ParallelProduct(this._data[i]), thread_pool);
+        }
+        return res;
+    }
+
+}
