1e5c164f8d5ca76d55006e3ffa882897f9a23739
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2022-12-28 18:03:49 +0300

    Update MatrixPolynomial.cs

--- MatrixPolynomial.cs
+++ MatrixPolynomial.cs
@@ -4,12 +4,43 @@ class MatrixPolynomial
     Класс Матричный полином
     */
 
-    private double[] _data = new double[0];
+    private double[] _data = new double[0] {};
 
     // 1. Конструктор полиномов
     public MatrixPolynomial(double[] coefficient)
     {
         this._data = coefficient;
+    }
+
+    static void PrintMatrix(Matrix matrix)
+    {
+        Console.Write("[");
+        for (var i = 0; i < matrix.GetDimension()[0]; i++)
+        {
+            if (i != 0)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("[");
+            for (var j = 0; j < matrix.GetDimension()[1]; j++)
+            {
+                Console.Write(matrix.Data[i, j].ToString().PadLeft(10));
+                if (j + 1 < matrix.GetDimension()[1])
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.Write("]");
+            if (i + 1 == matrix.GetDimension()[0])
+            {
+                Console.Write("]");
+            }
+            else
+            {
+                Console.Write(",");
+            }
+            Console.WriteLine();
+        }
     }
 
 
@@ -24,8 +55,10 @@ class MatrixPolynomial
         Matrix res = new Matrix(matrix.GetDimension()[0], matrix.GetDimension()[1]);
         InstanceThreadPool thread_pool = new InstanceThreadPool(4, Name: "Обработчик матриц");
         for (int i = 0; i < this._data.Length; i++)
-        {
-            res = res.ParallelSumThreadPool(res.ParallelPowBinThreadPool((uint)i, thread_pool).ParallelProduct(this._data[i]), thread_pool);
+        {            
+            res = res.ParallelSumThreadPool(matrix.ParallelPowBinThreadPool(i, thread_pool).ParallelProduct(this._data[this._data.Length - i - 1]), thread_pool);
+            
+            //Console.WriteLine($"Матрица: ");
         }
         return res;
     }
